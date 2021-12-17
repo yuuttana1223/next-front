74f96daaa2a7e5a4c4b2e6857698352cb244e4a4
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-17 20:05:57 +0900

    ➕ ✨ Tailwind CSSを導入

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "src/assets/styles/globals.css";
+import type { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
