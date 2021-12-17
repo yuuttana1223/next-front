c29230002a610f38aae85fc5e6a9231cf375921a
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-12-17 20:09:08 +0900

    🚚 ディレクトリ構造やファイルの場所を変更

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>KCG Review</title>
+        <meta
+          name="description"
+          content="京都コンピュータ学院(KCG)の授業評価サイトです"
+        />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
