de6dfbb0c6503efd95ef645b5e1350c4fe70df0d
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-01-15 21:59:44 +0900

    ♻️ ✨ 講師たちの名前と講義の名前を取得できるようにした
    hooksに入れたほうがコードがきれいになるから
    
    #8

--- src/hooks/useAllReviews.ts
+++ src/hooks/useAllReviews.ts
@@ -4,9 +4,25 @@ import { fetcher } from "src/utils/fetcher";
 import useSWRImmutable from "swr/immutable";
 
 export const useAllReviews = () => {
-  const { data, error } = useSWRImmutable<Review[], Error>(
-    `${API_URL}/reviews`,
-    fetcher
-  );
-  return { reviews: data, reviewsError: error, loading: !data && !error };
+  const { data: reviews, error: reviewsError } = useSWRImmutable<
+    Review[],
+    Error
+  >(`${API_URL}/reviews`, fetcher);
+  return {
+    reviews,
+    reviewsError,
+    loading: !reviews && !reviewsError,
+    // 重複した講義を削除(講義の種類)
+    lectures: reviews
+      ?.map((review) => review.lecture_name)
+      .filter(
+        (lectureName, index, self) => self.indexOf(lectureName) === index
+      ),
+    // 重複した講師を削除(講師の種類)
+    teachers: reviews
+      ?.map((review) => review.teacher_name)
+      .filter(
+        (teacherName, index, self) => self.indexOf(teacherName) === index
+      ),
+  };
 };
