a584702d6dc737482009e2ba3a18be1c4b5785fa
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-01-26 15:03:42 +0900

    🔒️ レビュー編集ページにアクセス制限 (#11)
    レビューを投稿した人以外が編集できないようにするため

--- src/pages/reviews/[id]/edit/index.tsx
+++ src/pages/reviews/[id]/edit/index.tsx
@@ -3,10 +3,12 @@ import Head from "next/head";
 import { AppLayout } from "src/layouts/AppLayout";
 import { EditReview as EditReviewComponent } from "src/components/Review/EditReview";
 import { ProtectUserRoute } from "src/routes/ProtectUserRoute";
+import { CorrectReviewUserRoute } from "src/routes/CorrectReviewUserRoute";
 
 const EditReview: NextPage = () => {
   return (
     <ProtectUserRoute>
+      <CorrectReviewUserRoute>
         <Head>
           <title>Edit KCG Review</title>
           <meta name="description" content="授業評価編集ページ" />
@@ -14,6 +16,7 @@ const EditReview: NextPage = () => {
         <AppLayout>
           <EditReviewComponent />
         </AppLayout>
+      </CorrectReviewUserRoute>
     </ProtectUserRoute>
   );
 };
