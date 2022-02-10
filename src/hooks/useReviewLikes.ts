aa49269de0dc490d060b0e19b42c394665a22152
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-02-10 13:53:30 +0900

    🚚 ファイル名をレビューのこととわかりやすくするために変更

--- src/hooks/useReviewLikes.ts
+++ src/hooks/useReviewLikes.ts
@@ -2,7 +2,7 @@ import useSWRImmutable from "swr/immutable";
 import { API_URL } from "src/urls/api";
 import { Like } from "src/apis/like";
 
-export const useLikes = (reviewId?: number | string | string[]) => {
+export const useReviewLikes = (reviewId?: number | string | string[]) => {
   const { data: likes, error: likesError } = useSWRImmutable<Like[], Error>(
     reviewId ? `${API_URL}/reviews/${reviewId}/likes` : null
   );
