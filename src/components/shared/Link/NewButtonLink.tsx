d1355d613b0ff89c9fd83c6fd0f5ed7a57139a4a
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-02-19 11:50:02 +0900

    💄 レビュー送信ボタンの色に合わせるように変更

--- src/components/shared/Link/NewButtonLink.tsx
+++ src/components/shared/Link/NewButtonLink.tsx
@@ -6,7 +6,7 @@ import { PATH } from "src/urls/path";
 export const NewButtonLink: VFC = () => {
   return (
     <Link href={PATH.REVIEWS.NEW}>
-      <a className="inline-block p-2 text-xl text-center text-white bg-green-400 hover:bg-green-500 rounded-3xl shadow-sm">
+      <a className="inline-block p-2 text-xl text-center text-white bg-emerald-400 hover:bg-emerald-500 rounded-full shadow-sm md:p-3 md:rounded-3xl">
         <HiOutlinePlusSm title="新規投稿ボタン" size={28} className="inline" />
         <span className="hidden align-bottom md:inline">新規投稿</span>
       </a>
