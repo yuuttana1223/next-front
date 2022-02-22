09211113da6f54c9690735f73990628fd07a3510
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-02-22 16:46:43 +0900

    ♿️ 矢印アイコンを逆に変更 (#35)

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,7 +1,7 @@
 import { Disclosure } from "@headlessui/react";
 import { VFC } from "react";
 import { LectureLink } from "src/components/shared/Link/LectureLink";
-import { HiChevronUp } from "react-icons/hi";
+import { HiChevronDown } from "react-icons/hi";
 import { Review } from "src/apis/review";
 
 type Props = {
@@ -19,13 +19,13 @@ export const Accordion: VFC<Props> = (props) => {
             ))}
           </Disclosure.Panel>
           <Disclosure.Button className="flex items-center py-2 w-full text-center hover:text-gray-800 hover:bg-gray-100 rounded">
-            <HiChevronUp
+            <HiChevronDown
               className={`${
                 open ? "transform rotate-180" : ""
               } w-7 h-7 inline `}
             />
             <span className="ml-4">
-              {open ? "折りたたむ" : `他${props.reviews?.length}件を表示`}
+              {open ? "折りたたむ" : `他 ${props.reviews?.length} 件を表示`}
             </span>
           </Disclosure.Button>
         </>
