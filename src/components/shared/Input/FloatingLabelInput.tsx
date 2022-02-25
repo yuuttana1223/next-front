e2ee4187a2d9ed8d6974644a6205f10eca116d2b
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-02-25 22:13:06 +0900

    ♿️ 目のアイコンを押すとパスワードが見れるように変更 (#2)
    パスワードを間違えて登録するのを防ぐため

--- src/components/shared/Input/FloatingLabelInput.tsx
+++ src/components/shared/Input/FloatingLabelInput.tsx
@@ -1,5 +1,7 @@
-import { VFC, useState, useEffect } from "react";
+import { VFC, useState, useEffect, useCallback } from "react";
 import { useFormContext } from "react-hook-form";
+import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi";
+import { EyeButton } from "src/components/shared/Button/EyeButton";
 
 type Props = {
   type: string;
@@ -18,11 +20,31 @@ type Props = {
 export const FloatingLabelInput: VFC<Props> = (props) => {
   const [active, setActive] = useState(false);
   const { register, setValue } = useFormContext();
+  const [inputState, setInputState] = useState({
+    isActive: false,
+    isVisible: false,
+    type: props.type,
+  });
+
+  const handleClick = useCallback(() => {
+    setInputState((prevInputState) => {
+      return {
+        ...prevInputState,
+        isVisible: !prevInputState.isVisible,
+        type: prevInputState.isVisible ? "password" : "text",
+      };
+    });
+  }, []);
 
   useEffect(() => {
     if (props.value) {
       setValue(props.name, props.value);
-      setActive(true);
+      setInputState((prevInputState) => {
+        return {
+          ...prevInputState,
+          isActive: true,
+        };
+      });
     }
   }, [props.name, props.value, setValue]);
 
@@ -45,6 +67,16 @@ export const FloatingLabelInput: VFC<Props> = (props) => {
         >
           {props.placeholder}
         </label>
+        {props.type === "password" &&
+          (inputState.isVisible ? (
+            <EyeButton onClick={handleClick}>
+              <HiOutlineEyeOff size="24px" />
+            </EyeButton>
+          ) : (
+            <EyeButton onClick={handleClick}>
+              <HiOutlineEye size="24px" />
+            </EyeButton>
+          ))}
       </div>
     </div>
   );
