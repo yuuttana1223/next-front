a2496276e268397f336902c5295d0d26c730b5c8
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-04 01:06:59 +0900

    🔍 SEO改善

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from "src/providers/AuthProvider";
 import { Toaster } from "react-hot-toast";
 import { SWRConfig } from "swr";
 import axios from "axios";
+import NextHeadSeo from "next-head-seo";
 
 const fetcher = (url: string) => {
   return axios
@@ -16,12 +17,27 @@ const fetcher = (url: string) => {
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <AuthProvider>
-      <SWRConfig value={{ fetcher }}>
-        <Component {...pageProps} />
-      </SWRConfig>
-      <Toaster toastOptions={{ duration: 5000 }} />
-    </AuthProvider>
+    <>
+      <NextHeadSeo
+        title="kcg-review"
+        canonical="https://kcg-review.vercel.app"
+        description="京都コンピュータ学院の授業評価サイトです。気に入った授業を見つけて有意義な時間を手に入れましょう"
+        og={{
+          image: "school_building_ekimae.png",
+          type: "website",
+          siteName: "kcg-review",
+        }}
+        twitter={{
+          card: "summary",
+        }}
+      />
+      <AuthProvider>
+        <SWRConfig value={{ fetcher }}>
+          <Component {...pageProps} />
+        </SWRConfig>
+        <Toaster toastOptions={{ duration: 5000 }} />
+      </AuthProvider>
+    </>
   );
 };
 
