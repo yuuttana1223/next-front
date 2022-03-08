7afe1ee4a87a37177c36dd39c6cb6e96fb933fdf
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-08 14:55:17 +0900

    🧑‍💻 iPhoneで開発環境を見れるようにした

--- src/urls/api.ts
+++ src/urls/api.ts
@@ -1,4 +1,8 @@
+// export const API_URL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://kcg-review.herokuapp.com/api/v1"
+//     : "http://localhost:3001/api/v1";
 export const API_URL =
   process.env.NODE_ENV === "production"
     ? "https://kcg-review.herokuapp.com/api/v1"
-    : "http://localhost:3001/api/v1";
+    : "http://192.168.0.114:3001/api/v1";
